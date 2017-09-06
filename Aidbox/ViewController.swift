d66e30578ab71b95065bf25cb72fbd29ad12a443
Eugene Kalyada <Evgeniy.Kalyada@waveaccess.ru>
2017-09-06 11:33:05 +0500

    Initial Commit

---
+++ Aidbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aidbox
+//
+//  Created by Eugene Kalyada on 06.09.17.
+//  Copyright © 2017 Eugene Kalyada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

549196ed53342ed3d13ca49b7c3e59ecd186a04d
Eugene Kalyada <Evgeniy.Kalyada@waveaccess.ru>
2017-09-07 18:04:32 +0500

    git ignore

---
+++ Aidbox/Managers/UserManager.swift
@@ -0,0 +1,13 @@
+//
+//  UserManager.swift
+//  Aidbox
+//
+//  Created by Eugene Kalyada on 07.09.17.
+//  Copyright © 2017 Eugene Kalyada. All rights reserved.
+//
+
+import UIKit
+
+class UserManager: NSObject {
+
+}
---
+++ Aidbox/Object/User/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Aidbox
+//
+//  Created by Eugene Kalyada on 07.09.17.
+//  Copyright © 2017 Eugene Kalyada. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
